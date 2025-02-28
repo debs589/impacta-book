9631cf7a760fe1b59b64955973a7f49096637645
debs589 <debora.carvalho589@gmail.com>
2025-02-28 18:15:14 -0300

    create account/user page

--- impacta-book-web/src/handlers/pages_handler.go
+++ impacta-book-web/src/handlers/pages_handler.go
@@ -9,3 +9,7 @@ func LoadLoginPage(w http.ResponseWriter, r *http.Request) {
 	utils.RenderTemplate(w, "login.html", nil)
 
 }
+
+func LoadUserRegisterPage(w http.ResponseWriter, r *http.Request) {
+	utils.RenderTemplate(w, "user-register.html", nil)
+}
