18d2c05eb645d05f381e40c59cfe0c3368dd93de
debs589 <debora.carvalho589@gmail.com>
2025-03-04 19:35:42 -0300

    adjust of User struct and creation of interfaces

--- impacta-book-api/internal/models/User.go
+++ impacta-book-api/internal/models/User.go
@@ -5,8 +5,16 @@ import "time"
 type User struct {
 	ID        uint64    `json:"id,omitempty"`
 	Name      string    `json:"name,omitempty"`
-	Nickname  string    `json:"username,omitempty"`
+	Nickname  string    `json:"nickname,omitempty"`
 	Email     string    `json:"email,omitempty"`
 	Password  string    `json:"password,omitempty"`
 	CreatedAt time.Time `json:"created_at,omitempty"`
+}
+
+type UserService interface {
+	CreateUser(user *User) error
+}
+
+type UserRepository interface {
+	CreateUser(user *User) error
 }
