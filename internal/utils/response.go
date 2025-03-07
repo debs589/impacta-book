cf73caf7fd8c7c024d8e68f8e27fcd71b9ecd9ef
debs589 <debora.carvalho589@gmail.com>
2025-03-07 16:24:57 -0300

    refactore of response package

--- internal/utils/response.go
+++ internal/utils/response.go
@@ -2,68 +2,27 @@ package utils
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
-type ErrorResponse struct {
-	Status  string `json:"status"`
-	Message string `json:"message"`
-}
-
-func Error(w http.ResponseWriter, statusCode int, message string) {
-	defaultStatusCode := http.StatusInternalServerError
-	// check if status code is valid
-	if statusCode > 299 && statusCode < 600 {
-		defaultStatusCode = statusCode
-	}
-
-	body := ErrorResponse{
-		Status:  http.StatusText(defaultStatusCode),
-		Message: message,
-	}
-	bytes, err := json.Marshal(body)
-
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(defaultStatusCode)
-
-	if _, err = w.Write(bytes); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-}
-
-type successResponse struct {
-	Data any `json:"data"`
+func Error(w http.ResponseWriter, statusCode int, err error) {
+	JSON(w, statusCode, struct {
+		Error string `json:"error"`
+	}{
+		Error: err.Error(),
+	})
 }
 
 func JSON(w http.ResponseWriter, code int, body any) {
-	// check body
-	if body == nil {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(code)
-		return
-	}
 
-	response := successResponse{body}
-	bytes, err := json.Marshal(response)
-
-	if err != nil {
-		// default error
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	// set header (before code due to it sets by default "text/plain")
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 
-	if _, err = w.Write(bytes); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+	if body != nil {
+		if err := json.NewEncoder(w).Encode(body); err != nil {
+			log.Fatal(err)
+		}
 	}
+
 }
